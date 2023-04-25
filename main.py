230be5e54625eb4715c8ee5358c00fdbf1278e69
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-25 14:25:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 
-
+import random 
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World, by Nandha"}
+    string = {"hello?"}
+    return string
+
+
+from resources.nekos import nekos
+
+@app.get("/nekos") # to get random nekos images
+async def nekos():
+    nekos = random.choice(nekos)
+    string = {"url": nekos}
+    return string 
+     
