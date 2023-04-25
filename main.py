afe919173893aae5f55028ab7bbca7ff8fae3cae
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-25 15:05:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    string = {"welcome to nandha-api"}
+    string = "welcome to Nandha-Api"
     return string
 
 
-from resources.nekos import nekos
-
-@app.get("/nekos") # to get random nekos images
-async def nekos():
-    nekos = random.choice(nekos)
-    string = {"url": nekos}
+@app.get("/neko")
+async def Nekos():
+    string = {"url": "example.url"}
     return string 
      
