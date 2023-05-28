3fadc65b50a801d7b0773f5b4a68fb212c7494ff
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-28 20:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
-import random, strings , resources
+import random, strings
+
+from resources import nekos, games
 
 
 from fastapi import FastAPI
@@ -8,19 +10,20 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return strings.HOME
+    return {"HOME = "WELCOME TO NANDHA-API"}
+
 
 
 @app.get("/neko")
 async def neko():
-    url = random.choice(resources.nekos.neko)
+    url = random.choice(nekos.neko)
     string = {"url": url}
     return string
 
 
 @app.get("/ward")
 async def ward():
-     ward_list = resources.games.COMMON_WARDS
+     ward_list = games.COMMON_WARDS
      random.shuffle(ward_list)
      ward = random.choice(ward_list)
      chars = list(ward)
