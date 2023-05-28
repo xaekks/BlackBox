c8a47668f1a83b370ee7454fe77f5f1c275851ec
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-28 20:03:32 +0530

    Update games.py

--- resources/games.py
+++ resources/games.py
@@ -1,16 +1,37 @@
 
 
+# expanded list of 200 commonly used words:
 
-GUESS_WARD = [
-   'ball', 'cat', 'boy', 'girl',
-   'bruh', 'nandha', 'vegeta', 'goku',
-   'sky', 'game', 'people', 'man',
-   'woman', 'fuck', 'dog', 'cute',
-   'anime', 'telegram', 'guess', 'what',
-   'right', 'wrong', 'nice', 'thanks',
-   'cool', 'youtube', 'discord', 'python',
-   'love', 'hate', 'sorry', 'cry', 'neko',
-   'knowledge', 'book', 'read', 'write', 'song', 'child', 'great', 'scam'
+COMMON_WORDS = [
+    'the', 'of', 'and', 'a', 'to', 'in', 'is', 'it', 'that', 'for', 
+    'you', 'was', 'with', 'on', 'be', 'they', 'at', 'have', 'this', 
+    'from', 'or', 'had', 'by', 'not', 'but', 'we', 'an', 'which', 
+    'one', 'would', 'all', 'will', 'there', 'say', 'who', 'make', 'when',
+    'can', 'more', 'if', 'no', 'man', 'out', 'other', 'so', 'what',
+    'time', 'up', 'go', 'about', 'than', 'into', 'could', 'state', 'only',
+    'new', 'year', 'some', 'take', 'come', 'these', 'know', 'see', 'use',
+    'get', 'like', 'then', 'first', 'any', 'work', 'now', 'may', 'such',
+    'give', 'over', 'think', 'most', 'even', 'find', 'day', 'also', 'after',
+    'way', 'many', 'must', 'look', 'before', 'great', 'back', 'through', 'long',
+    'where', 'much', 'should', 'well', 'people', 'down', 'own', 'just', 'because',
+    'good', 'each', 'those', 'feel', 'seem', 'how', 'high', 'too', 'place',
+    'little', 'world', 'very', 'still', 'nation', 'hand', 'old', 'life', 'tell',
+    'write', 'become', 'here', 'show', 'house', 'both', 'between', 'need', 'mean',
+    'same', 'end', 'another', 'left', 'while', 'might', 'next', 'under', 'never',
+    'give', 'own', 'every', 'hear', 'new', 'must', 'think', 'very', 'day',
+    'school', 'most', 'great', 'learn', 'before', 'high', 'life', 'last', 'long',
+    'point', 'right', 'low', 'little', 'different', 'small', 'large', 'number', 'off',
+    'away', 'place', 'year', 'ask', 'such', 'even', 'few', 'try', 'open',
+    'night', 'example', 'always', 'run', 'move', 'team', 'minute', 'important', 'until',
+    'information', 'working', 'line', 'product', 'end', 'member', 'keep', 'hold', 'household',
+    'offer', 'know', 'name', 'pass', 'main', 'available', 'order', 'true', 'story',
+    'military', 'account', 'west', 'girl', 'family', 'player', 'usual', 'sea', 'major',
+    'side', 'kind', 'provide', 'terms', 'federal', 'chairman', 'appear', 'heart', 'union',
+    'reason', 'continue', 'decide', 'site', 'sound', 'note', 'help', 'picture', 'clear',
+    'practice', 'piece', 'land', 'water', 'upon', 'policy', 'usually', 'book', 'different',
+    'class', 'increase', 'natural', 'notice', 'cover', 'several', 'cause', 'speak', 'period',
+    'strong', 'action', 'lay', 'school', 'lead', 'take', 'study', 'plant', 'example',
+    'food', 'thought', 'second', 'better', 'special', 'person', 'develop', 'sense', 'mind'
 ]
    
 
