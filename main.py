f5a477a53cb6103d252f5c9efbb209818dc9c1ca
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-03-28 14:53:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ import strings
 import requests
 import cleverbotfreeapi
 import json
+import secureme
 
-from resources import anime, game
+
+from resources import anime, game, quote
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
 app = FastAPI()
+
+
+
+class CodeRequest(BaseModel):
+    code: str
+    language: str
+
+
 
 @app.get("/", include_in_schema=False)
 def serve_index():
@@ -18,13 +28,52 @@ def serve_index():
     return FileResponse(index_file)
 
 
+@app.get("quote", tags=['quote'])
+async def anime_quote():
+   url = random.choice(quote.anime_quote_url)
+   return { "url": url }
+    
 @app.get("/neko", tags=['images'])
 async def neko():
     url = random.choice(anime.neko)
-    string = {"url": url}
-    return string
+    return {"url": url}
 
 
+
+def run(code, language):
+    res = requests.get("https://emkc.org/api/v2/piston/runtimes")
+    langs = next((lang for lang in res.json() if lang["language"] == language), None)
+
+    if langs is not None:
+        data = {
+            "language": language,
+            "version": langs["version"],
+            "files": [
+                {
+                    "name": f"file.{langs['aliases'][0] if langs['aliases'] else 'xz'}",
+                    "content": code,
+                },
+            ],
+        }
+
+        r = requests.post("https://emkc.org/api/v2/piston/execute", json=data)
+
+        if r.status_code == 200:
+            return {
+                "language": r.json()["language"],
+                "version": r.json()["version"],
+                "code": data["files"][0]["content"].strip(),
+                "output": r.json()["run"]["output"].strip()
+            }
+        else:
+            raise HTTPException(status_code=r.status_code, detail=f"Status Text: {r.reason}")
+    else:
+        raise HTTPException(status_code=404, detail="Error: language is not found.")
+
+@app.post("/run")
+async def run_code(code_request: CodeRequest):
+    return run(code_request.code, code_request.language)
+    
 
 
 
@@ -54,7 +103,22 @@ async def ai_models(model: str , prompt: str):
 
 
 
-@app.get("/word", tags=['tools'])
+@app.get('/encrypt/{string}', tags=['tools'])
+async def encrypt(string: str):
+     text = secureme.encrypt(string)
+     encryption = json.dumps({'encrypt': text})
+     return encryption 
+
+@app.get('/decrypt/{string}', tags=['tools'])
+async def encrypt(string: str):
+     text = secureme.decrypt(string)
+     decryption = json.dumps({'decrypt': text})
+     return decryption 
+     
+    
+
+    
+@app.get("/guess", tags=['tools'])
 async def ward():
      words_list = game.COMMON_WORDS
      random.shuffle(words_list)
