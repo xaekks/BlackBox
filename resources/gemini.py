b8b2c07cb0bb5314ba0f3a4a9c45949e2ec35b83
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-08 15:12:11 +0530

    Create gemini.py

---
+++ resources/gemini.py
@@ -0,0 +1,50 @@
+
+from pydantic import BaseModel
+
+import requests
+import json
+
+API_KEY = "AIzaSyCV7FjStkDviA_Evd20rNRoB1dL5kDQzeg"
+api_url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
+
+
+class Gemini(BaseModel):
+     text: str
+     role: str
+
+
+def GeminiFunc(text: str, role: str):
+      headers = {
+    "Content-Type": "application/json"
+}
+
+      data = {
+    "contents": [
+        {
+            "parts": [
+                {"text": text}
+            ],
+            "role": "User"
+        }
+    ],
+    "system_instruction": {
+        "parts": [
+            {"text": role}
+        ],
+        "role": "model"
+    }
+}
+      response = requests.post(url, headers=headers, data=json.dumps(data))
+      data = response.json()
+  
+      if date.get('candidates'):
+            return {
+              "reply": data['candidates'][0]['content']['parts'][0]['text']
+            }
+      else:
+            return {
+             "error": data
+           }
+
+        
+    
