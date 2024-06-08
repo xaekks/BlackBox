97866c48beedc2f23d03ff243fb5442eaf910b88
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-08 17:01:33 +0530

    Update gemini.py

--- resources/gemini.py
+++ resources/gemini.py
@@ -5,15 +5,31 @@ import json
 API_KEY = "AIzaSyCV7FjStkDviA_Evd20rNRoB1dL5kDQzeg"
 API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
 
+
 class Gemini(BaseModel):
     text: str
     role: str
 
 def gemini_func(text: str, role: str):
-    data = Gemini(text=text, role=role)
     headers = {"Content-Type": "application/json"}
-  
-    response = requests.post(API_URL, headers=headers, json=data.dict())
+    payload = {
+    "contents": [
+        {
+            "parts": [
+                {"text": text}
+            ],
+            "role": "User"
+        }
+    ],
+    "system_instruction": {
+        "parts": [
+            {"text": role}
+        ],
+        "role": "model"
+    }
+}
+
+    response = requests.post(API_URL, headers=headers, json=payload)
     data = response.json()
   
     if data.get('candidates'):
