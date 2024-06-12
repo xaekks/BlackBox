adc32180b363a51c642014ee98b32677c2f385af
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-13 01:12:06 +0530

    Update tools.py

--- resources/tools.py
+++ resources/tools.py
@@ -68,37 +68,6 @@ async def zerochan(string: str):
     return False
 
 
-async def run(code, language):
-    res = requests.get("https://emkc.org/api/v2/piston/runtimes")
-    langs = next((lang for lang in res.json() if lang["language"] == language), None)
-
-    if langs is not None:
-        data = {
-            "language": language,
-            "version": langs["version"],
-            "files": [
-                {
-                    "name": f"file.{langs['aliases'][0] if langs['aliases'] else 'xz'}",
-                    "content": code,
-                },
-            ],
-        }
-
-        r = requests.post("https://emkc.org/api/v2/piston/execute", json=data)
-
-        if r.status_code == 200:
-            return {
-                "language": r.json()["language"],
-                "version": r.json()["version"],
-                "code": data["files"][0]["content"].strip(),
-                "output": r.json()["run"]["output"].strip()
-            }
-        else:
-            raise HTTPException(status_code=r.status_code, detail=f"Status Text: {r.reason}")
-    else:
-        raise HTTPException(status_code=404, detail="Error: language is not found.")
-        
-
 async def get_urbandict(word, max=10):
     response = requests.get(f"http://api.urbandictionary.com/v0/define?term={quote(word)}")
     if response.status_code == 200:
